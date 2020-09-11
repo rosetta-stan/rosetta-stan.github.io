fa4cee34201dd6ffe92f260336e7bdbc4df421fa
breckbaldwin <git@lingpipe.com>
2020-09-11 17:15:50 -0400

    added new loading routines for skipping cache

--- usage/load_scholar.py
+++ usage/load_scholar.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
-"""
+Attempt to scrape scholar.google.com results for Stan usage tracking"""
 
 #from selenium import webdriver
 
@@ -13,6 +10,8 @@ import redis
 import re
 import numpy
 import time
+import subprocess
+import os
 
 REDIS = redis.Redis()
 
@@ -39,6 +38,7 @@ YEAR_END = 2020
 OFFSET_PATTERN = re.compile("start....(\d+)") # needs to be fixed, hacky
 GOOGLE_COUNT = 'google_count'
 FILTERED_COUNT = 'filtered_count'
+RAW_COUNT = 'raw_count'
 
 PKG_QUERY = ['rstan']
 
@@ -57,29 +57,57 @@ def cache_retrieve(query,headers):
         # print("hit cache: \n" + query)
     return page
 
+def cache_only(query,headers):
+    return REDIS.get(query)
+    
+
+def interactive_cache_retrieve(query,headers):
+    page = REDIS.get(query)
+    if (not page):
+        done = False
+        while (not done):
+            print("miss cache: \n" + query)
+            old_file_mod_time = os.stat('/Users/breck/git/rosetta-stan.github.io/usage/file.html').st_mtime
+            subprocess.call(['open','--wait-apps','--new',query]) # blocking
+        #subprocess.call(['open',query])
+            # answer = input("<ctl-c> to quit,<return> when /Users/breck/git/rosetta-stan.github.io/usage/file.html is saved")
+            new_file_mod_time = os.stat('/Users/breck/git/rosetta-stan.github.io/usage/file.html').st_mtime
+            if (old_file_mod_time - new_file_mod_time >= 0):
+                print("/Users/breck/git/rosetta-stan.github.io/usage/file.html has not changed")
+                continue;
+            f=open('/Users/breck/git/rosetta-stan.github.io/usage/file.html', "r")    
+            page = f.read()
+            f.close()
+            REDIS.set(query,page)
+            done = True
+    return page
+
 for i in range(0,len(PKG_QUERY)):
     package = PKG_QUERY[i]
     package_d = {}
     for year in range(YEAR_START,YEAR_END):
-        package_d[year] = {GOOGLE_COUNT:0,FILTERED_COUNT:0}
+        package_d[year] = {GOOGLE_COUNT:0,FILTERED_COUNT:0, RAW_COUNT:0}
         done = False     
         result_start_str = ''
         first_page= True
         filtered_count = 0
         while not done:
             query =  URL_PREFIX + result_start_str + "q=" + package + START_YEAR + str(year) + END_YEAR + str(year)
+            #page = cache_only(query,HEADERS)
+            #page = interactive_cache_retrieve(query,HEADERS)
             page = cache_retrieve(query,HEADERS)
+            if not page:
+                done = True
+                continue
             soup = BeautifulSoup(page, 'html.parser')
-            text = soup.get_text()
-            bounce_message = "Our systems have detected unusual traffic from your computer network"
-            if bounce_message in text:
-                print("bounced quers is: ",query)
-                break
+            
             count_match = soup.find(attrs={"id":"gs_ab_md"})
+            if not count_match:
+                raise ValueError("query got unexpected result:" + query)
             # <div id="gs_ab_md"><div class="gs_ab_mdw">Page 15 of about 137,000 results (
             if count_match and first_page:
                 google_count = count_match.get_text()
-                RESULTS_COUNT_PATTERN = re.compile("([\d,]+)") # About 3,339 results (0.03 sec)'
+                RESULTS_COUNT_PATTERN = re.compile("([\d,]+) result") # About 3,339 results (0.03 sec)'
                 google_count_value = RESULTS_COUNT_PATTERN.search(google_count).group(1)
                 package_d[year][GOOGLE_COUNT] = int(google_count_value.replace(',',''))
                 print("Got ",package_d[year][GOOGLE_COUNT]," estimated hits" )
@@ -88,6 +116,7 @@ for i in range(0,len(PKG_QUERY)):
                 print("too many results to page", package_d[year][GOOGLE_COUNT]," for query:", query);
                 break
             for mention in soup.find_all(attrs={"class": "gs_r gs_or gs_scl"}): #go through indivudual mentions
+                package_d[year][RAW_COUNT] += 1
                 abstract = mention.find(attrs={"class":"gs_rs"})
                 if abstract:
                     abs_text = mention.get_text()
@@ -99,8 +128,9 @@ for i in range(0,len(PKG_QUERY)):
                      #       print("skipping as part of 'understand':", abs_text_clean)
                             continue
                         rstan_token_p = re.compile('\srstan\s',re.IGNORECASE)
-                        if not rstan_token_p.search(abs_text):
-                      #      print("skipping for no token 'rstan':",abs_text)
+                        rstan_substr_p = re.compile('rstan',re.IGNORECASE)
+                        if rstan_substr_p.search(abs_text) and not rstan_token_p.search(abs_text):
+                            #print("skipping for no token 'rstan' but substring:",abs_text)
                             continue
                         package_d[year][FILTERED_COUNT] += 1
                         #print("kept:",abs_text)
@@ -111,7 +141,9 @@ for i in range(0,len(PKG_QUERY)):
                 result_start_str =  'start=' + result_start + '&'
             else:
                done = True
-        print("Package: ", package, " year: ", year, "google count: ", package_d[year][GOOGLE_COUNT], "filter count: ", package_d[year][FILTERED_COUNT])
+        print("Package:", package, "year: ", year, "google count:", 
+              package_d[year][GOOGLE_COUNT], "filter count:", package_d[year][FILTERED_COUNT],
+              "raw_count:",package_d[year][RAW_COUNT])
                
                #print("Package: ", package, " year: ", year, "count: ", package_d[year][FILTERED_COUNT])
 
