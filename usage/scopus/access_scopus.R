c6cb5eca9ddc9a29a8d3ec667f1d60fad1b292dd
breckbaldwin <git@lingpipe.com>
2020-09-28 12:10:08 -0400

    added in some more graphs for Sloan report around categories

--- usage/scopus/access_scopus.R
+++ usage/scopus/access_scopus.R
@@ -37,9 +37,15 @@ years <- year_start:year_end
 df <- data.frame(years)
 
 
-pkg_query <- c('http://mc-stan.org',
+pkg_query <- c('mc-stan.org+OR+rstan+OR+rstanarm',
                '',
                '')
+
+# https://www.scopus.com/results/results.uri?sort=plf-f&src=s&sid=972b41d3f2e99383aad2d6c684d7c5d8&sot=a&sdt=a&sl=20&s=mc-stan.org+OR+rstan&origin=searchadvanced&editSaveSearch=&txGid=a992c6224b315cb8549e80f316d7c26e
+
+#pkg_query <- c('pymc3','stan','rstan', 'tensorflow','pytorch',
+#               '','mc-stan.org', '','','',
+#               '','','','','')
 
 pkg_query_m <- matrix(pkg_query,ncol=3)
 
@@ -53,7 +59,7 @@ INSTITUTION_TOKEN <- credentials$INSTITUTION_TOKEN
 BASE_URL = 'https://api.elsevier.com/content/search/scopus'
 
 year_start <- 2011
-year_end <- 2019
+year_end <- 2020
 
 
 years <- year_start:year_end
@@ -73,7 +79,7 @@ for (i in 1:nrow(pkg_query_m) ) {
     j <- 1
     while (j < facet_count) {
       name <- data$`search-results`$facet$category$name[j]
-      #label <- data$`search-results`$facet$category$label[j]
+      name <- data$`search-results`$facet$category$label[j]
       hitCount <- as.numeric(data$`search-results`$facet$category$hitCount[j])
       if (!name %in% colnames(scopus.df)) {
         scopus.df[name] <- rep(0,year_end - year_start + 1)
@@ -91,8 +97,23 @@ df_long_label <- df_long %>%
   mutate(label=if_else(years == max(years), 
                        as.character(topic),NA_character_))
 
+"Economics, Econometrics and Finance"
+
+df_long_label2 <- df_long_label[str_detect(df_long_label$topic,"Finance|Business|Social|Psychology|Decision"),]
+plot2econ <- ggplot(data=df_long_label2,aes(x=years,y=yr_count,group=topic, color=topic)) + 
+  geom_line() +
+  geom_label_repel(aes(label = label),
+                   na.rm = TRUE) +
+  scale_color_discrete(guide = FALSE) +
+  scale_x_discrete(name="publication year",limits=c(2010:2020)) +
+  geom_point()
+
+
 plot2 <- ggplot(data=df_long_label,aes(x=years,y=yr_count,group=topic, color=topic)) + 
   geom_line() +
   geom_label_repel(aes(label = label),
                    na.rm = TRUE) +
-  scale_color_discrete(guide = FALSE)
+  scale_color_discrete(guide = FALSE) +
+  geom_point()
+
+print(plot2)
